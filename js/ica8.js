77cea1709bdfcea318cafa27448ffd100025efbc
Rafael Basurto <raba3672@colorado.edu>
2022-10-11 10:36:04 -0600

    Finished ICA8

---
+++ js/ica8.js
@@ -0,0 +1,18 @@
+
+let body = document.querySelector('body');
+
+document.getElementById("color").addEventListener("click", randomColor);
+function randomColor(){
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+}
+
+const btn = document.getElementById('message');
+btn.addEventListener('click', function handleClick(){
+    btn.textContent = 'The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start. Also, happy hannukah ';
+});
