eae8c86942278201776b97c5720c460a54076372
Rafael Basurto <raba3672@colorado.edu>
2022-10-21 14:28:24 -0600

    Completed assignment9

--- js/assignment9.js
+++ js/assignment9.js
@@ -5,23 +5,34 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imgNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const imgNames = ['pic6.jpg', 
+'pic7.jpg', 
+'pic8.jpg', 
+'pic9.jpg', 
+'pic10.jpg',];
 /* Declaring the alternative text for each image file */
 const altText = {
-    'pic1.jpg': 'Closeup of a human eye',
-    'pic2.jpg': 'Marble stone',
-    'pic3.jpg': 'Brunfelsia flower',
-    'pic4.jpg': 'An egyptian wall painting',
-    'pic5.jpg': 'A butterfly on a leaf'
+    'pic6.jpg': 'Peter Griffin',
+    'pic7.jpg': 'Stan Smith',
+    'pic8.jpg': 'Roger Smith',
+    'pic9.jpg': 'Stewie Griffin',
+    'pic10.jpg': 'Glenn Quagmire'
 }
 /* Looping through images */
 function imageThumbBar(){
-    for(let i=0; i<imagesNames.length; i++){
+    for(let i=0; i<imgNames.length; i++){
         const newImage = document.createElement('img');
         newImage.setAttribute('src', `../img/assignment9_img/${imgNames[i]}`);
+        newImage.setAttribute('alt', altText[`${imgNames[i]}`]);        
+        thumbBar.appendChild(newImage);
+        newImage.addEventListener('click', (e) => {
+            displayedImage.src = e.target.src;
+            displayedImage.alt = e.target.alt;
+        })
     }
 }
-imageThumbBar();
+
+imageThumbBar()
 /*const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
@@ -33,7 +44,8 @@ btn.addEventListener('click', () => {
         btn.setAttribute('class', 'light');    
         btn.textContent = 'Lighten';
         overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    }else {
+    }
+    else {
         btn.setAttribute('class', 'dark');
         btn.textContent = 'Darken';
         overlay.style.backgroundColor = 'rgba(0,0,0,0)';
