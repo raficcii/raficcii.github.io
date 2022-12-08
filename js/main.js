84fa9cf57943915879193532cd4cda0d38829201
Rafael Basurto <raba3672@colorado.edu>
2022-12-07 20:59:15 -0700

    Complete Portfolio, Fully Functional

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Get the elements with class="column"
-var elements = document.getElementsByClassName("column");
+var elements = document.getElementsByClassName('column');
 
 // Declare a "loop" variable
 var i;
@@ -12,15 +12,30 @@ function one() {
 }
 
 // Two images side by side
-function two() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "50%";
-  }
-}
-
-// Four images side by side
 function four() {
   for (i = 0; i < elements.length; i++) {
     elements[i].style.flex = "25%";
   }
 }
+
+// Code By Webdevtrick ( https://webdevtrick.com )
+function beforeAfter() {
+    document.getElementById('compare').style.width = document.getElementById('slider').value + "%";
+}
+// End WebdevTrick Code
+
+function changeOne(){
+    document.querySelector('figure').style.backgroundImage = "url(../img/quince2bw.jpg)";
+    document.getElementById('compare').style.backgroundImage = "url(../img/quince2.jpg)";
+}
+
+function changeTwo(){
+    document.querySelector('figure').style.backgroundImage = "url(../img/senior1.jpg)";
+    document.getElementById('compare').style.backgroundImage = "url(../img/senior1bw.jpg)";
+}
+
+function changeThree(){
+    document.querySelector('figure').style.backgroundImage = "url(../img/other6.jpg)";
+    document.getElementById('compare').style.backgroundImage = "url(../img/other6bw.jpg)";
+}
+
